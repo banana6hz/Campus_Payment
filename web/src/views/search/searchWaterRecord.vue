aff00ce413aa330f00e54bf11c28bcd513236f0a
lhz666 <liuhz@bbdtek.com>
2019-04-22 18:08:43 +0800

    record

--- web/src/views/search/searchWaterRecord.vue
+++ web/src/views/search/searchWaterRecord.vue
@@ -1,52 +1,61 @@
 <template>
     <div>
-        <record :recordDatas='recordData' :table-label="tableLabel"></record>
+        <record :recordDatas="recordData" :table-label="tableLabel" :total="total"></record>
     </div>
 </template>
 <script>
     import record from './record'
+    import axios from 'axios'
     export default {
         data () {
             return {
-                recordData: [
-                    {index: '0',buyTun: '10',restTun: '20',money:'30',restMoney:'60',paymentTime:'2018-10-10'},
-                    {index: '1',buyTun: '20',restTun: '20',money:'60',restMoney:'60',paymentTime:'2019-03-01'},
-                    {index: '0',buyTun: '10',restTun: '20',money:'30',restMoney:'60',paymentTime:'2018-10-10'},
-                    {index: '1',buyTun: '20',restTun: '20',money:'60',restMoney:'60',paymentTime:'2019-03-01'},
-                    {index: '0',buyTun: '10',restTun: '20',money:'30',restMoney:'60',paymentTime:'2018-10-10'},
-                    {index: '1',buyTun: '20',restTun: '20',money:'60',restMoney:'60',paymentTime:'2019-03-01'},
-                    {index: '0',buyTun: '10',restTun: '20',money:'30',restMoney:'60',paymentTime:'2018-10-10'},
-                    {index: '1',buyTun: '20',restTun: '20',money:'60',restMoney:'60',paymentTime:'2019-03-01'},
-                    {index: '0',buyTun: '10',restTun: '20',money:'30',restMoney:'60',paymentTime:'2018-10-10'},
-                    {index: '1',buyTun: '20',restTun: '20',money:'60',restMoney:'60',paymentTime:'2019-03-01'},
-                    {index: '0',buyTun: '10',restTun: '20',money:'30',restMoney:'60',paymentTime:'2018-10-10'},
-                    {index: '1',buyTun: '20',restTun: '20',money:'60',restMoney:'60',paymentTime:'2019-03-01'},
-                    {index: '0',buyTun: '10',restTun: '20',money:'30',restMoney:'60',paymentTime:'2018-10-10'},
-                    {index: '1',buyTun: '20',restTun: '20',money:'60',restMoney:'60',paymentTime:'2019-03-01'},
-                    {index: '0',buyTun: '10',restTun: '20',money:'30',restMoney:'60',paymentTime:'2018-10-10'},
-                    {index: '1',buyTun: '20',restTun: '20',money:'60',restMoney:'60',paymentTime:'2019-03-01'}],
+                recordData: [],
                 tableLabel: [
-                    {label:'序号', param: 'index',width:'6%'},
-                    {label:'交易时间', param:'paymentTime',width:'14%'},
-                    {label:'剩余金额（元）', param: 'restMoney',width:'15%'},
-                    {label:'剩余吨数(吨）', param: 'restTun',width:'15%'},
-                    {label:'充值金额（元）', param: 'money',width:'15%'},
-                    {label:'充值吨数(吨）', param: 'buyTun',width:'15%'},
-                    {label:'余额（元）', param: 'restMoney',width:'15%'},
-                    {label:'可用吨数(吨）', param: 'restTun',width:'15%'}],
+                {label:'序号', param: 'index',width:'6%'},
+                {label:'交易时间', param:'paymentTime',width:'14%'},
+                {label:'剩余金额（元）', param: 'restMoney',width:'15%'},
+                {label:'剩余吨数(吨）', param: 'restTun',width:'15%'},
+                {label:'充值金额（元）', param: 'money',width:'15%'},
+                {label:'充值吨数(吨）', param: 'buyTun',width:'15%'},
+                {label:'余额（元）', param: 'restMoney',width:'15%'},
+                {label:'可用吨数(吨）', param: 'tun',width:'15%'}],
                 total: 0,
-                currentPage: 0,
+                currentPage: 1,
                 current: 0,
                 pageSizes: 5,
-                pageSize: 5
+                pageSize: 5,
+                showPagination: true
             }
         },
         components: {
             record
         },
+        mounted() {
+            this.getWaterRecord()
+        },
         methods: {
             handleCurrentChange: function(val) {
                 console.log('当前页：' + val);
+                this.getWaterRecord()
+            },
+            // 获取水费记录
+            getWaterRecord(){
+                axios.get(`/api/users/waterRecord?size=${this.pageSize}&page=${this.currentPage}`).then(response=>{
+                let res = response.data;
+                if(res.status==='0'){
+                    this.recordData = res.result.waterRecord;
+                    console.log(this.recordData)
+                    this.total = Number.parseInt(res.result.total);
+                    if(this.total <= this.pageSize){
+                        this.showPagination = false
+                    }
+                }else{
+                    console.log(res.msg);    
+                }
+                }).catch(err=>{
+                    console.log(err);
+                    
+                })
             }
         }
     }
